f066e997b88a88381d62809e271a11d3af117b0f
anautin <anautin@uno.edu>
2020-03-26 13:27:49 -0500

    added additional comments to js file to explain the sequence of actions the program goes through as the user interacts with the web app

--- behavior.js
+++ behavior.js
@@ -1,62 +1,35 @@
 
+// 1. ESTABLISH REFERENCE POINTS
 // create references to DOM elements we need to manipulate
 var enterButton = document.getElementById("enter");	//getting enter button
-var input = document.getElementById("userInput");	//getting data
+var input = document.getElementById("userInput");	//getting user text 
 var ul = document.querySelector("ul");	//getting the unordered list
 var item = document.getElementsByTagName("li");	//getting the list item
 
 
 
-// GETTER
-// size of the to-do that the user input into text box
-function inputLength(){
-	return input.value.length;
-} 
 
 
-// GETTER
-// how many to-do items on the TO-DO list
-function listLength(){
-	return item.length;
-}
 
+// 2. CREATE LISTENERS FOR SOME OF THOSE REFERENCE POINTS
+// listen for user activity with the ENTER button and keyboard keystrokes (ENTER key)
+// then, invoke necessary functions to respond accordingly
+//------------------------------------------------------//
+enterButton.addEventListener("click",addListAfterClick);	//listen for click on "enterButton"
 
-// how we can tack on a new item to our TO-DO list
-function createListElement() {
-	var li = document.createElement("li"); // creates an element "li"
-	li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
-	ul.appendChild(li); //adds li to ul
-	input.value = ""; //Reset text input field
-
-
-	//START STRIKETHROUGH
-	// because it's in the function, it only adds it for new items
-	function crossOut() {
-		li.classList.toggle("done");
-	}
-
-	li.addEventListener("click",crossOut);
-	//END STRIKETHROUGH
-
-
-	// ADD THE DELETE BUTTON
-	var dBtn = document.createElement("button");
-	dBtn.appendChild(document.createTextNode("X"));
-	li.appendChild(dBtn);
-	dBtn.addEventListener("click", deleteListItem);
-	// END ADD DELETE BUTTON
-
-
-	//ADD CLASS DELETE (DISPLAY: NONE)
-	function deleteListItem(){
-		li.classList.add("delete")
-	}
-	//END ADD CLASS DELETE
-}
+input.addEventListener("keypress", addListAfterKeypress);	//listen for keypress inside "userInput" field
+//------------------------------------------------------//
 
 
 
 
+
+
+
+// 3. RESPOND TO USER INTERACTING WITH REFERENCE POINTS
+// after using mouse to click on 'Add' button OR by using keyboard to press ENTER
+// validate user input before proceeding 
+//------------------------------------------------------//
 function addListAfterClick(){
 	if (inputLength() > 0) { //makes sure that an empty input field doesn't create a li
 		createListElement();
@@ -69,9 +42,67 @@ function addListAfterKeypress(event) {
 		createListElement();
 	} 
 }
+//------------------------------------------------------//
 
 
-enterButton.addEventListener("click",addListAfterClick);	//listen for click on "enterButton"
 
-input.addEventListener("keypress", addListAfterKeypress);	//listen for keypress inside "userInput" field
+
+
+
+// 4. CREATE LIST ITEM FOR NEW TASK
+//------------------------------------------------------//
+function createListElement() {
+	var li = document.createElement("li"); // creates new instance of list item
+	li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
+	ul.appendChild(li); //adds li to ul
+	input.value = ""; //Reset text input field
+
+
+	// COMPLETED TASK
+	// because it's in the function, it only adds it for new items
+	function crossOut() {
+		li.classList.toggle("done");	//using the DOM to toggle the "done" style class
+	}
+
+	li.addEventListener("click",crossOut);
+	// END COMPLETED TASK
+
+
+
+
+	// ADD THE DELETE BUTTON TO THE LIST ITEM
+	var dBtn = document.createElement("button");
+	dBtn.appendChild(document.createTextNode("X"));	//makes text using our X input
+	li.appendChild(dBtn);
+	
+	// RESPOND TO DELETE CLICK
+	dBtn.addEventListener("click", deleteListItem);
+	
+
+
+	//ADD CLASS DELETE (DISPLAY: NONE)
+	function deleteListItem(){
+		li.classList.add("delete")
+	}
+	//END ADD CLASS DELETE
+}
+//------------------------------------------------------//
+
+
+
+
+
+// GETTER/HELPER METHODS
+//------------------------------------------------------//
+// size of the to-do that the user inputs into text box
+function inputLength(){	
+	return input.value.length;
+} 
+
+// how many to-do items on the TO-DO list
+function listLength(){
+	return item.length;
+}
+//------------------------------------------------------//
+
 
